5b24836d00960de30d8866790e665c778cb360a1
allan <allanrey.macasaquit@gmail.com>
2024-11-26 21:21:40 -0500

    3. Set up Backend using Convex docs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Inter } from 'next/font/google';
-
+import { ConvexAuthNextjsServerProvider } from '@convex-dev/auth/nextjs/server';
+import { ConvexClientProvider } from '@/components/ConvexClientProvider';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -15,8 +16,12 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang='en'>
-			<body className={inter.className}>{children}</body>
-		</html>
+		<ConvexAuthNextjsServerProvider>
+			<html lang='en'>
+				<body className={inter.className}>
+					<ConvexClientProvider>{children}</ConvexClientProvider>
+				</body>
+			</html>
+		</ConvexAuthNextjsServerProvider>
 	);
 }
